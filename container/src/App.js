401942200f14c1ad2631f53deb6fa0fe00363a6e
Prakash7143 <kumarprakash7143@gmail.com>
2022-12-16 00:24:10 +0530

    header added & App.js updated for css-in-jsx

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from './components/MarketingApp';
+import { 
+    StylesProvider, 
+    createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "container"
+});
 
 export default () => {
     return (
-        <div>
-            <h3>Container App MicroServices New</h3>
-            <hr/>
-            <MarketingApp/>
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <h3>Container App MicroServices New</h3>
+                    <hr/>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
+        
     )
 }
